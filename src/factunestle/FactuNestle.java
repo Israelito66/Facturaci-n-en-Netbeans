54f8f2a66c7d09582abde2560e81644ba811890a
Usuario <Usuario@TOSHIBA>
2016-05-24 20:16:01 -0500

    FacturaDesarrollo2

--- src/factunestle/FactuNestle.java
+++ src/factunestle/FactuNestle.java
@@ -1,8 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+ * UNIANDES
+ * Ingenieria en Sistemas 
+ * Jonsthan Salguero
+ * 9no Semestre 
+*/
 
 package factunestle;
 
